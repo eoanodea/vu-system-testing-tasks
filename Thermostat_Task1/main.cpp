bfdd078d36761cbed40537cc50620787b94318d7
alireza Soltani nezhad <78270828+alirezasolataninezhad@users.noreply.github.com>
2022-05-20 01:08:38 +0200

    Updated file for thermostat, comments also added

--- Thermostat_Task1/main.cpp
+++ Thermostat_Task1/main.cpp
@@ -1,57 +1,51 @@
-#include "mbed.h"
-#include "board_freedom.h"
-#include "adc.h"
-#include "oled_ssd1322.h"
-#include <cstdint>
-#include <cstdio>
-#include <array>
-float read_temperature()
-{
-float const voltage = (adc_read(0) / 65535.0f) * 3.0f;
-return (voltage * 1000 - 400) / 19.5f;
+
+int main(){
+    initDisplay();
+    DigitalOut redLed(PTB2);
+    DigitalOut greenLed(PTB3);
+    PwmOut heaterPower(PTC2);
+
+    while (true){
+        float const temperature = readTemperature(); //reading temperature by converting 16 bit value
+
+        if temperature >= -10
+            bool sensorPresent = true;
+        else
+            bool sensorPresent = false;
+        constexpr float minAllowedTemp = 30.0f;
+        constexpr float minAllowedTemp = 35.0f;
+        if (sensorPresent && temperature >= minAllowedTemp && temperature <= minAllowedTemp) // both 30 and 35 also include in bindery  30<= temperature <=35
+            bool isTemperatureRange = true;
+        else
+            bool isTemperatureRange = false;
+        constexpr float averageRangeTemperature = 32.5f; // This number use to keep temperature in the middle of allowed range
+
+        if (sensorPresent && temperature < averageRangeTemperature)
+           heaterPower = true;
+        else
+           heaterPower = false;
+
+        if (temperature>35)
+            bool overHeating  = true;
+        else
+            bool overHeating  = false;
+
+
+        if(heaterPower == true) ////The red LED is ON when heating system is working.
+            redLed = true;
+        else //The red LED is PFF when heating systme is not working
+            redLed = false;
+        if(isTemperatureRange == true) //The green LED is ON when the measured temperature is in the specified range.
+            greenLed = true;
+        else //The green LED is OFF when the measured temperature is in not the specified range.
+            greenLed = false;
+        if(overHeating == true)
+            heaterPower = false;
+
+        if (temperature>=-10.0f)  //FR-02-Display Temperature: The program has an option to show the plate's temperature with a heating sensor and display it on an LCD display for users.
+            displaySensorTemperature(temperature);
+        else //FR-07-Heating Sensor: It is expected that the system will be protected for the condition that the heating sensors is absent.
+            displayErrorMessage("Temperature sensor is absent.");
+        ThisThread::sleep_for(500ms);
+    }
 }
-void init_display()
-{
-board_init();
-u8g2_ClearBuffer(&oled);
-u8g2_SetFont(&oled, u8g2_font_6x12_mr);
-u8g2_SendBuffer(&oled);
-}
-void display_message(char const *message)
-{
-u8g2_ClearBuffer(&oled);
-u8g2_DrawUTF8(&oled, 10, 10, message);
-u8g2_SendBuffer(&oled);
-}
-void display_temperature(float temp)
-{
-std::array<char, 50> msg {};
-// workaround because %f format specifier is not supported
-int const ti = static_cast<int>(temp);
-int const tf = abs(static_cast<int>(100 * temp) % 100);
-std::snprintf(msg.data(), msg.size(), "Temperature is %d.%02d C", ti, tf);
-display_message(msg.data());
-}
-int main()
-{
-init_display();
-DigitalOut led_green {PTB3};
-DigitalOut led_red {PTC12};
-PwmOut heater_power {PTA7};
-while (true)
-{
-float const temp = read_temperature();
-constexpr float SENSOR_CUTOFF = -10.0f;
-bool const has_sensor = temp >= SENSOR_CUTOFF;
-constexpr float TEMP_MIN = 30.0f;
-constexpr float TEMP_MAX = 35.0f;
-led_green = has_sensor && temp >= TEMP_MIN && temp <= TEMP_MAX;
-constexpr float TEMP_TARGET = 32.5f;
-heater_power = has_sensor && temp < TEMP_TARGET;
-led_red = heater_power;
-if (has_sensor) display_temperature(temp);
-else display_message("No temperature sensor connected!");
-ThisThread::sleep_for(100ms);
-}
-}
-Tests
